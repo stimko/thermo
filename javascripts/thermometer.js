d886b4981c14a0817a142886568c733f37f6004e
Stephen Timko <s.w.timko@gmail.com>
2014-02-02 16:52:03 -0500

    wip

--- javascripts/thermometer.js
+++ javascripts/thermometer.js
@@ -24,14 +24,14 @@ Thermometer.prototype = {
 
   getCurrentColor: function() {
     if ((this.getCurrentDegrees() * 4) >= 60) {
-      this.currentR += .5;
+      this.currentR += 1;
       this.currentG -= 1;
-      this.currentB -= .5;  
+      this.currentB -= 1;  
 
     } else {
-      this.currentR += 1;
+      this.currentR += 1.2;
       this.currentG += .5;
-      this.currentB -= .5;  
+      this.currentB -= 1;  
     }
     return "rgb(" + Math.floor(this.currentR) + "," + Math.floor(this.currentG) + "," + Math.floor(this.currentB) +")";
   },
