0b2c15df1d0f40f4bdf0e6e1282a1455aac224ac
Stephen Timko <s.w.timko@gmail.com>
2014-02-08 18:46:51 -0500

    raindrop styling

--- javascripts/conditions.js
+++ javascripts/conditions.js
@@ -1,21 +1,25 @@
 Conditions = {
   "rain": function(){
-    context.lineWidth = 6;
-    context.strokeStyle = 'white';
+    context.lineWidth = 4;
+    context.strokeStyle = 'rgb(75,182,214)';
+    context.fillStyle = 'rgb(96,212,247)';
     context.beginPath();
     context.moveTo(280, 180);
     context.bezierCurveTo(240, 210, 300, 220, 280, 180);
     context.closePath();
+    context.fill();
     context.stroke();
     context.beginPath();
-    context.moveTo(230, 210);
-    context.bezierCurveTo(190, 240, 250, 250, 230, 210);
+    context.moveTo(230, 200);
+    context.bezierCurveTo(190, 230, 250, 240, 230, 200);
     context.closePath();
+    context.fill();
     context.stroke();
     context.beginPath();
     context.moveTo(255, 140);
     context.bezierCurveTo(215, 170, 275, 180, 255, 140);
     context.closePath();
+    context.fill();
     context.stroke();
   },
   "cloudy": function(){},
